a210a0f678ca9b2fd0da31315d97f4bcc2c3ce9b
catherinekulchitskaya-arch <catherinekulchitskaya@gmail.com>
2025-12-16 23:13:08 +0300

    Update postrussia.h

--- postrussia.h
+++ postrussia.h
@@ -1,21 +1,28 @@
 #ifndef POST_H
 #define POST_H
+
+#define TRACKING_LEN 20
 #define NAME_LEN 100
+#define EMAIL_LEN 100
+#define ADDRESS_LEN 200
+#define STATUS_LEN 50
+#define DATE_LEN 11
+#define PHONE_LEN 20
 
 typedef struct {
-    char tracking_number;    
-    char sender;           
-    char recipient;
-    char sender_number;
-    char recipient_number;
-    char sender_email;
-    char recipient_email;
-    char sender_address;   
-    char recipient_address; 
-    char status;           
-    char date;              
-    float weight;               
-    float cost;                 
+    char tracking_number[TRACKING_LEN];    
+    char sender[NAME_LEN];                 
+    char recipient[NAME_LEN];              
+    char sender_phone[PHONE_LEN];          
+    char recipient_phone[PHONE_LEN];       
+    char sender_email[EMAIL_LEN];          
+    char recipient_email[EMAIL_LEN];       
+    char sender_address[ADDRESS_LEN];      
+    char recipient_address[ADDRESS_LEN];   
+    char status[STATUS_LEN];               
+    char date[DATE_LEN];                   
+    float weight;                          
+    float cost;                            
 } PostalItem;
 
 typedef struct Node {
@@ -23,21 +30,28 @@ typedef struct Node {
     struct Node* next;
 } Node;
 
+
 Node* create_node(PostalItem item);
 void add_to_list(Node** head, PostalItem item);
-void delete_from_list(Node** head, char tracking_number );
-Node* find_by_id(Node* head, char tracking_number);
-Node* find_by_status(Node* head, const char* status);
+int delete_from_list(Node** head, const char* tracking_number);
 void print_list(Node* head);
 void free_list(Node** head);
 int list_size(Node* head);
 
+
+Node* find_by_tracking_number(Node* head, const char* tracking_number);
+Node* find_by_status(Node* head, const char* status);
+
+
 void save_to_file(Node* head, const char* filename);
 Node* load_from_file(const char* filename);
 
-PostalItem create_item();
-void edit_item(PostalItem* item);
+ create_item_interactive();
+void edit_item_interactive(PostalItem* item);
 void print_item(PostalItem item);
 
+
+Node* find_by_sender(Node* head, const char* sender_name);
+Node* find_by_recipient(Node* head, const char* recipient_name);
 
 #endif
